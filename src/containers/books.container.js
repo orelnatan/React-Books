d3d268266cbe0f47d7e643fa599014189ccb6e4e
orelnatan <natenorel@gmail.com>
2018-07-09 22:33:48 +0300

    update3

--- src/containers/books.container.js
+++ src/containers/books.container.js
@@ -16,4 +16,7 @@ const mapDispatchToProps = (dispatch) => {
                               }, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Books)
+export const Books = connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Books)
